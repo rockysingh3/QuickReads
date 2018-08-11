fe17f42ebce01eeeab0203fdc45df0abdfbe16e5
rockysingh3 <rockygsingh@icloud.com>
2018-08-11 18:31:33 -0400

    Added request lib and fatched google book api

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,17 @@
 'use strict';
 const router = require('express').Router();
+const request = require('request');
 
-router.get('/', (req, res) => {
-  res.render('home');
+
+
+router.get('/', (req, res, next) => {
+  request('https://www.googleapis.com/books/v1/volumes?q=harry+potter', (err, response, body) => {
+    if(!err && response.statusCode == 200){
+      let apiResults = JSON.parse(body);
+      res.render('home');
+    }
+  });
+  //res.render('home');
 });
 
 
