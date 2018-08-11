c8eba1cd2d0d22d010eaac9a952e4e349f1c29d5
rockysingh3 <rockygsingh3@gmail.com>
2018-08-11 06:46:43 -0400

    project structure

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const homeRoute = require('./routes/home');
+
+
+
+app.set('port',process.env.PORT || 3000);
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+
+const PORT = 3000;
+
+
+app.use('/', homeRoute.router);
+
+
+app.listen(app.get('port'), () => {
+  console.log("The app is running on port ", app.get('port'));
+});
