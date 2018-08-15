ff2e88d13f8b5c0b198c91820e38b7ecee471cdd
rockysingh <rockygsingh@icloud.com>
2018-08-14 21:06:50 -0400

    added cong for body-parser

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Routes
 const homeRoute = require('./routes/home');
