c94285e3825b96a4ca0caf27e46f8f498a14a979
rockysingh <rockygsingh@icloud.com>
2018-08-15 23:48:42 -0400

    deleted body-parser config, didn't need it

--- routes/bookDetails.js
+++ routes/bookDetails.js
@@ -3,14 +3,6 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const request = require('request');
-const bodyParser = require('body-parser');
-
-
-/// you need get the ID here
-// so you can make another api call
-// with the id in the url
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
 
 
 
