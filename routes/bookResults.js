96d9e9dab13e71d16454321a0c4174fdd285d29e
rockysingh3 <rockygsingh@icloud.com>
2018-08-19 04:34:22 -0400

    incresed the query search to 40 books per page

--- routes/bookResults.js
+++ routes/bookResults.js
@@ -7,7 +7,7 @@ const request = require('request');
 
 router.get('/bookResults', (req, res) => {
   let bookQuery = req.query.searchForBook;
-  request('https://www.googleapis.com/books/v1/volumes?q=' + bookQuery, (err, response, body) => {
+  request('https://www.googleapis.com/books/v1/volumes?q=' + bookQuery + '&maxResults=40&orderBy=relevance', (err, response, body) => {
     if(!err && response.statusCode == 200){
       let apiResultsdata = JSON.parse(body);
       res.render('bookResults', {apiResultsdata: apiResultsdata});
