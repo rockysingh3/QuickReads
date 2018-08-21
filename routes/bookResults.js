3910d990838d53cae5cb76afbeafce915d6ce9cc
rockysingh3 <rockygsingh@icloud.com>
2018-08-21 02:06:28 -0400

    updated the route

--- routes/bookResults.js
+++ routes/bookResults.js
@@ -6,24 +6,34 @@ const request = require('request');
 
 
 router.get('/bookResults', (req, res) => {
-  let bookQuery = req.query.searchForBook;
-  request('https://www.googleapis.com/books/v1/volumes?q=' + bookQuery + '&maxResults=40&orderBy=relevance', (err, response, body) => {
-    if(!err && response.statusCode == 200){
-      let apiResultsdata = JSON.parse(body);
-      res.render('bookResults', {apiResultsdata: apiResultsdata});
-    }
-  });
+    // quering for string typed in input field on the home page
+    let bookQuery = req.query.searchForBook;
+    // making call to google books api with a search word
+    const URL = 'https://www.googleapis.com/books/v1/volumes?q=' + bookQuery + '&maxResults=40&orderBy=relevance';
+    request(URL, (err, response, body) => {
+          if (!err && response.statusCode == 200) {
+              let apiResultsdata = JSON.parse(body);
+              res.render('bookResults', { apiResultsdata: apiResultsdata });
+          } else {
+              console.log(err);
+          }
+    });
 });
 
 
 router.get('/books/:id', (req, res) => {
-  let id =  req.params.id;
-  request('https://www.googleapis.com/books/v1/volumes/' + id, (err, response, body) => {
-    if(!err && response.statusCode == 200){
-      let bookdata = JSON.parse(body);
-      res.render('bookDetails', {bookdata: bookdata});
-    }
-  });
+    // getting the id of one book from bookResults page
+    let id = req.params.id;
+    //making a call to google books api with a ID of one book 
+    const URL = 'https://www.googleapis.com/books/v1/volumes/' + id;
+    request(URL, (err, response, body) => {
+          if (!err && response.statusCode == 200) {
+              let bookdata = JSON.parse(body);
+              res.render('bookDetails', { bookdata: bookdata });
+          } else {
+              console.log(err);
+          }
+    });
 });
 
 
